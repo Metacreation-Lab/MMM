c39cdd91346ca559f29754e87e1db1e6bcef2562
Paul Triana <paul_triana@sfu.ca>
2025-03-26 14:35:09 +0100

    install correct MidiTok

--- slurm/install_dependencies.sh
+++ slurm/install_dependencies.sh
@@ -17,7 +17,7 @@ module load gcc arrow/17.0.0 rust  # needed since arrow can't be installed in th
 source .venv/bin/activate
 
 srun bash -c "pip install symusic==0.5.0"
-srun bash -c "pip install git+https://github.com/Natooz/MidiTok"
+srun bash -c "pip install git+https://github.com/DaoTwenty/MidiTok@expressive"
 srun bash -c "pip install transformers accelerate tensorboard"
 srun bash -c "pip install flash_attn"
 srun bash -c "pip install deepspeed==0.14.4"
